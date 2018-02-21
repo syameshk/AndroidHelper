2535c9d4df404d9bd7df1c815868f9c15f4ac3c1
US\skunnummal <skunnummal@deloitte.com>
2018-02-21 14:28:27 +0530

    Initial commit

---
+++ app/src/main/java/deloitte/androidhelper/MainActivity.java
@@ -0,0 +1,59 @@
+package deloitte.androidhelper;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import deloitte.sensorhelper.HeartRateSensor;
+
+public class MainActivity extends Activity {
+
+    public HeartRateSensor sensor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sensor = new HeartRateSensor();
+        sensor.Init(getApplicationContext());
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                update();
+            }
+        },1000,5000);
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d("Sensor","Starting");
+        super.onResume();
+        sensor.Start();
+    }
+
+    @Override
+    protected void onStop() {
+        sensor.Stop();
+        super.onStop();
+    }
+
+    private void update(){
+        //This method is called directly by the timer
+        //and runs in the same thread as the timer.
+
+        Log.d("Sensor","Update :"+sensor.getCurrentData());
+
+        //We call the method that will work with the UI
+        //through the runOnUiThread method.
+    }
+
+
+
+}
---
+++ sensorhelper/src/main/java/deloitte/sensorhelper/HeartRateSensor.java
@@ -0,0 +1,57 @@
+package deloitte.sensorhelper;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.content.Context;
+import android.util.Log;
+
+/**
+ * Created by skunnummal on 2/21/2018.
+ */
+
+public class HeartRateSensor {
+    private SensorManager sensorManager;
+    private Sensor sensor;
+
+    private float heatRate = 0;
+
+    public void Init(Context context){
+        sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE);
+    }
+
+    public void Start(){
+        sensorManager.registerListener(sensorListener, sensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE), SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    public void Stop(){
+        sensorManager.unregisterListener(sensorListener);
+    }
+
+    public float getCurrentData(){
+        return heatRate;
+    }
+
+    private final SensorEventListener sensorListener = new SensorEventListener() {
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            /*
+            for(int i=0;i< event.values.length;i++){
+                Log.d("Sensor",i+" : "+event.values[i]);
+            }
+            */
+
+            heatRate = event.values[0];
+
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+
+    };
+
+}
